380ce7d18016ccc3508a14276be54509eb43a547
Jeremy Winter <jeremywinter@users.noreply.github.com>
2024-01-17 13:36:19 -0600

    update
    removed var

--- get-column-index.js
+++ get-column-index.js
@@ -1,10 +1,10 @@
 function getColumnIndex() {
 
   // gets column index
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   
   // enter column and row
-  var columnIndex = sheet.getRange("A1").getColumn();
+  const columnIndex = sheet.getRange("A1").getColumn();
   Logger.log("Column Index is: " + columnIndex);
   
 }
